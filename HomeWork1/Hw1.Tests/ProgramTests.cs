8d2b300ec290e18e4657fe0520ca247b2f66dd95
Рамиль Зарипов <zaripoff.ramil@gmail.com>
2022-09-12 14:57:37 +0300

    Added calculator in Program.cs and tested it

---
+++ HomeWork1/Hw1.Tests/ProgramTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace Hw1.Tests;
+
+public class ProgramTests
+{
+    [Theory]
+    [InlineData("1 + 2")]
+    [InlineData("1 / 2")]
+    [InlineData("1 - 2")]
+    [InlineData("0 / 0")]
+    [InlineData("1 / 0")]
+    public void ProgramDoesNotFallOnMathOperations(string input)
+    {
+        var result = Program.Main(input.Split());
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1 2")]
+    [InlineData("1")]
+    [InlineData("1 . 3 5")]
+    [InlineData("1 +")]
+    public void ProgramCatchesWrongNumberOfArguments(string input)
+    {
+        var result = Program.Main(input.Split());
+        Assert.Equal(-1, result);
+    }
+
+    [Theory]
+    [InlineData(". + 2")]
+    [InlineData("a - f")]
+    public void ProgramCatchesNonDoubleInput(string input)
+    {
+        var result = Program.Main(input.Split());
+        Assert.Equal(-1, result);
+    }
+
+    [Theory]
+    [InlineData("3 . 4")]
+    public void ProgramCatchesWrongOperations(string input)
+    {
+        var result = Program.Main(input.Split());
+        Assert.Equal(-2, result);
+    }
+}
--- HomeWork1/Hw1/Program.cs
+++ HomeWork1/Hw1/Program.cs
@@ -1,5 +1,24 @@
-﻿var arg1 = args[0];
-var operation = args[1];
-var arg2 = args[2];
-var result = arg1 + operation + arg2;
-Console.WriteLine(result);
+﻿namespace Hw1;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        try{
+            Parser.ParseCalcArguments(args, out var arg1, out var operation, out var arg2);
+            var result = Calculator.Calculate(arg1, operation, arg2);
+            Console.WriteLine(result);
+            return 0;
+        }
+        catch (Exception e)
+        {
+            return e switch
+            {
+                ArgumentOutOfRangeException => -3,
+                InvalidOperationException => -2,
+                ArgumentException => -1,
+                _ => throw e
+            };
+        }
+    }
+}
