4f5c60a5bfb72c61852db7ea8e67f184eda7f62b
ZaripovRamil <zaripoff.ramil@gmail.com>
2022-10-01 21:47:38 +0300

    Added singleton

--- Homework3/Hw3.Tests/SingleInitializationSingleton.cs
+++ Homework3/Hw3.Tests/SingleInitializationSingleton.cs
@@ -7,7 +7,9 @@ public class SingleInitializationSingleton
 {
     private static readonly object Locker = new();
 
-    private static volatile bool _isInitialized = false;
+    private static Lazy<SingleInitializationSingleton> _lazyHolder = new(()=>new SingleInitializationSingleton());
+
+    private static volatile bool _isInitialized;
 
     public const int DefaultDelay = 3_000;
     
@@ -16,20 +18,26 @@ public class SingleInitializationSingleton
     private SingleInitializationSingleton(int delay = DefaultDelay)
     {
         Delay = delay;
-        // imitation of complex initialization logic
         Thread.Sleep(delay);
     }
 
     internal static void Reset()
     {
-        throw new NotImplementedException();
+        _isInitialized = false;
+        _lazyHolder = new Lazy<SingleInitializationSingleton>(()=>new SingleInitializationSingleton());
     }
 
     public static void Initialize(int delay)
     {
-        throw new NotImplementedException();
+        if (_isInitialized) throw new InvalidOperationException();
+        lock (Locker)
+        {
+            if (_isInitialized) throw new InvalidOperationException();
+            _lazyHolder = new Lazy<SingleInitializationSingleton>(()=>new SingleInitializationSingleton(delay));
+            _isInitialized = true;
+        }
     }
 
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
+    public static SingleInitializationSingleton Instance => _lazyHolder.Value;
 
 }
