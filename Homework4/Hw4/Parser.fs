7d0db9bb6343259a18053329dd628f4ce8cd0e55
ZaripovRamil <zaripoff.ramil@gmail.com>
2022-10-08 14:09:10 +0300

    Added Hw4

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -4,17 +4,32 @@ open System
 open Hw4.Calculator
 
 
-type CalcOptions = {
-    arg1: float
-    arg2: float
-    operation: CalculatorOperation
-}
+type CalcOptions =
+    { arg1: float
+      arg2: float
+      operation: CalculatorOperation }
 
-let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+let isArgLengthSupported (args: string []) = args.Length = 3
 
-let parseOperation (arg : string) =
-    NotImplementedException() |> raise
-    
-let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+let parseOperation (arg: string) =
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> ArgumentException() |>raise
+
+let parseDouble (str:string) =
+    match Double.TryParse str  with
+    | true,double -> Some double
+    | _ -> ArgumentException()|>raise
+
+
+
+let parseCalcArguments (args: string []) =
+   if args = null || not(isArgLengthSupported args)
+   then ArgumentException()|> raise
+   else 
+    {arg1 = (parseDouble args[0]).Value
+     operation = parseOperation args[1]
+     arg2 = (parseDouble args[2]).Value}
--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,12 @@
-﻿raise (System.NotImplementedException())
+﻿open System
+open Hw4.Parser
+open Hw4.Calculator
+open Hw4
+
+let readArgs = Console.ReadLine().Split()
+let Calculate (calcOptions:CalcOptions):float =
+    calculate calcOptions.arg1 calcOptions.operation calcOptions.arg2
+
+
+Console.Write (readArgs |> parseCalcArguments |> Calculate)
+
